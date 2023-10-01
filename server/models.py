53fd2cfdefb29600f2b30980b6283ba770eccbb8
Joy Olago <joydanielle62@gmail.com>
2023-10-01 20:03:23 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -7,3 +7,10 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Plant {self.name} | Price: {self.price}>'
